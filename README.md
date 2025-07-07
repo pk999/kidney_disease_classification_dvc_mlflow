f1921d887c05c36be23b890a11521fcc20c6855c
praneeth <praneeth7853@gmail.com>
2025-07-07 08:28:49 +0530

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # kidney_disease_classification_dvc_mlflow
+# Amazon ECR URI - location Mumbai
+183035153668.dkr.ecr.ap-south-1.amazonaws.com/kidney_classification
---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+ 
+CONFIG_FILE_PATH = Path(r"C:\Users\nikhi\OneDrive\Desktop\kidney_disease_classification_dvc_mlflow\config\config.yaml")
+PARAMS_FILE_PATH = Path(r"C:\Users\nikhi\OneDrive\Desktop\kidney_disease_classification_dvc_mlflow\params.yaml")
